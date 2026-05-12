961ac5764acc53f06e6e32be173afa59e0f3cfef
Hanzla <hanzlainam204@gmail.com>
2026-05-13 02:45:36 +0500

    fixed start.sh

--- start.sh
+++ start.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-echo "Training model..."
+echo "🚀 Training model..."
 python src/train.py
 
-echo "Starting FastAPI backend..."
+echo "🚀 Starting FastAPI backend..."
 uvicorn api.main:app --host 0.0.0.0 --port 8000 &
 
-echo "Waiting for backend..."
-sleep 5
+echo "⏳ Waiting for backend..."
+sleep 15
 
-echo "Starting Streamlit dashboard..."
+echo "🚀 Starting Streamlit dashboard..."
 streamlit run dashboard/app.py \
     --server.port 7860 \
     --server.address 0.0.0.0 \
